f99d5a173fe42ec1f4efc8cfdf936a58b29b4ea5
William Ortega <william.ortega@outlook.com>
2015-07-05 23:52:09 -0600

    Configuration for local development

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,8 +1,8 @@
 <?php
 define('_DB_SERVER_', 'localhost');
 define('_DB_NAME_', 'bubumotosps');
-define('_DB_USER_', 'mybubumoto');
-define('_DB_PASSWD_', 'vThd16t');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');

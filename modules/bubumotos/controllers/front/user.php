d8394fa2a4c7e041411b972cc524bcc344783f2f
William Ortega <william.ortega@outlook.com>
2015-07-20 23:33:46 -0600

    Send new password from email password

--- modules/bubumotos/controllers/front/user.php
+++ modules/bubumotos/controllers/front/user.php
@@ -8,7 +8,8 @@ class bubumotosuserModuleFrontController extends ModuleFrontController
         foreach ($customers as $c) {
             // avoid that admin changes the psw
             if ((int)$c['id_customer'] > 1) {
-                $customer = Customer::getByEmail($c['email']);
+                $customer = new Customer();
+                $customer->getByemail($c['email']);
                 $customer->passwd = Tools::encrypt($password = Tools::passwdGen(MIN_PASSWD_LENGTH));
                 $customer->last_passwd_gen = date('Y-m-d H:i:s', time());
                 // send email with the new password to each customer
@@ -21,10 +22,7 @@ class bubumotosuserModuleFrontController extends ModuleFrontController
                         '{firstname}' => $customer->firstname,
                         '{passwd}' => $password
                     );
-                    /*if (Mail::Send($this->context->language->id, 'password', Mail::l('Your new password'), $mail_params, $customer->email, $customer->firstname.' '.$customer->lastname))
-                        $this->context->smarty->assign(array('confirmation' => 1, 'customer_email' => $customer->email));
-                    else
-                        $this->errors[] = Tools::displayError('An error occurred while sending the email.');*/
+                    Mail::Send((int)Configuration::get('PS_LANG_DEFAULT'), 'password', Mail::l('Your new password'), $mail_params, $customer->email, $customer->firstname.' '.$customer->lastname);
                 }
             }
         }
